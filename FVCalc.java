76989109dcd2c53c116ccecc86d6e3fd147ca3a1
razzantkeren <raz.zantkeren@post.runi.ac.il>
2024-11-07 17:14:53 +0200

    kkaa

--- FVCalc.java
+++ FVCalc.java
@@ -6,9 +6,7 @@ public class FVCalc {
 		double p =Double.parseDouble(args[1]);
 		int y =Integer.parseInt(args[2]);
 		double tp = 1+ (double)p/100 ;
-		System.out.println(tp);
 		double revach = Math.pow(tp, y);
-		System.out.println(revach);
 		double totalafter = revach*total;
 		System.out.println("After "+ y + " years, a $" + total + " saved at " + p +"% will yield $" + ((int)totalafter)  );
 
